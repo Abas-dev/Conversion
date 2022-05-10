71eb8690f19538e00d65e46093c091f727c90b2a
abas <abasisaac22@gmail.com>
2022-05-10 20:07:55 +0100

    completed the last class "binaryFormat"

--- src/project/BinaryFormat.java
+++ src/project/BinaryFormat.java
@@ -1,5 +1,23 @@
 package project;
 
-public class BinaryFormat {
+import java.util.Scanner;
 
+public class BinaryFormat {
+    public void binaryFormat(){
+        Scanner sc = new Scanner(System.in); // creating an object of the scanner class
+        try { // exception handling
+            System.out.print("Enter a number : ");
+            int n = sc.nextInt(); // receiving user binary input
+            int len = 32;
+            String binary = "";
+            for (long i = (1L << len - 1); i > 0; i = i / 2) {
+                binary += (n & i) != 0 ? "1" : "0";
+            }
+            System.out.println("The binary format of " + n + " is " + binary);
+        }catch (Exception e){ // catching exception
+            System.out.println("Pls input a number...");
+            System.out.println();
+            binaryFormat(); // recursion
+        }
+    }
 }
--- src/project/main.java
+++ src/project/main.java
@@ -8,48 +8,53 @@ public class main { //main class
             System.out.println();
             System.out.println("Select what you would like to convert : ");
             Scanner sc = new Scanner(System.in); // using java scanner class to receive user input
-            System.out.println("Enter 1 for Binary to Decimal");
-            System.out.println("Enter 2 for Decimal to Binary");
-            System.out.println("Enter 3 for Decimal to Hexadecimal");
-            System.out.println("Enter 4 for Hexadecimal to Decimal") ;
-            System.out.println("Enter 5 for Binary to Hexadecimal");
-            System.out.println("Enter 6 for Hexadecimal to Binary");
-            System.out.println("Enter 7 for Binary addition");
-            System.out.println("Enter 8 to Exit the program");
+            System.out.println("Enter 1 for Binary format of a number");
+            System.out.println("Enter 2 for Binary to Decimal");
+            System.out.println("Enter 3 for Decimal to Binary");
+            System.out.println("Enter 4 for Decimal to Hexadecimal");
+            System.out.println("Enter 5 for Hexadecimal to Decimal") ;
+            System.out.println("Enter 6 for Binary to Hexadecimal");
+            System.out.println("Enter 7 for Hexadecimal to Binary");
+            System.out.println("Enter 8 for Binary addition");
+            System.out.println("Enter 9 to Exit the program");
             System.out.println();
             System.out.print("Your input : "); // expecting user input
             try { // exception handling
                 int input = sc.nextInt(); // receiving user input and storing it in a variable
                 switch (input) { // using switch statement
                     case 1:
+                        BinaryFormat binaryFormat = new BinaryFormat();// creating an object of the class
+                        binaryFormat.binaryFormat();// using the object created to access the method in the class
+                        break;
+                    case 2:
                         BinaryToDecimal binaryToDecimal = new BinaryToDecimal(); // creating an object of the class
                         binaryToDecimal.binaryToDecimal();// using the object created to access the method in the class
                         break;
-                    case 2:
+                    case 3:
                         DecimalToBinary decimalToBinary = new DecimalToBinary(); // creating an object of the class
                         decimalToBinary.decimalToBinary();// using the object created to access the method in the class
                         break;
-                    case 3:
+                    case 4:
                         DecimalToHex decimalToHex = new DecimalToHex(); // creating an object of the class
                         decimalToHex.decimalToHex();// using the object created to access the method in the class
                         break;
-                    case 4:
+                    case 5:
                         HexToDecimal hexToDecimal = new HexToDecimal(); // creating an object of the class
                         hexToDecimal.hexToDecimal();// using the object created to access the method in the class
                         break;
-                    case 5:
+                    case 6:
                         BinaryToHex binaryToHex = new BinaryToHex(); // creating an object of the class
                         binaryToHex.binaryToHex();// using the object created to access the method in the class
                         break;
-                    case 6:
+                    case 7:
                         HexToBinary hexToBinary = new HexToBinary(); // creating an object of the class
                         hexToBinary.hexToBinary();// using the object created to access the method in the class
                         break;
-                    case 7:
+                    case 8:
                         BinaryAddition binaryAddition = new BinaryAddition();// creating an object of the class
                         binaryAddition.binaryAddition();// using the object created to access the method in the class
                         break;
-                    case 8:
+                    case 9:
                         System.exit(0); // to exit the program
                     default:
                         System.out.println("You did not select any of the above choices");
